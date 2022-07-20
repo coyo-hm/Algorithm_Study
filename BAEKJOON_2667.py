f27daf9c24dac3fc00b8dc72476aae5271c26374
DADA YO <dayeon.yoon@gmail.com>
2022-07-20 23:52:36 +0900

    BAEKJOON: 2667

--- BAEKJOON_2667.py
+++ BAEKJOON_2667.py
@@ -1,24 +1,38 @@
 import collections
 
 N = int(input())
-P1, P2 = map(int, input().split())
-M = int(input())
-graph = [[] for _ in range(N + 1)]
-v = [0] * (N + 1)
+graph = []
+com=[]
+v = [[False]*N]*N
 
-for _ in range(M):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
+for _ in range(N):
+  graph.append(list(map(int, list(input()))))
 
-def bfs(start):
-  q = collections.deque([start])
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs(y, x):
+  c = []
+  q = collections.deque([(y, x)])
   while q:
-    node = q.popleft()
-    for i in graph[node]:
-      if v[i] == 0:
-        v[i] = v[node] + 1
-        q.append(i)
-  
-bfs(P1)
-print(v[P2] if v[P2] > 0 else -1)
+    a, b = q.popleft()
+    c.append((a, b))
+    for i in range(4):
+      ny = a + dy[i]
+      nx = b + dx[i]
+      if(0 <= ny < N and 0 <= nx < N):
+        if(graph[ny][nx] == 1):
+          graph[ny][nx] = 0
+          q.append((ny, nx))
+  return len(c)
+
+for y in range (N):
+  for x in range (N):
+    if(graph[y][x] == 1): 
+      graph[y][x] = 0
+      com.append(bfs(y, x))
+    
+com.sort()
+print(len(com))
+for i in com:
+  print(i)
