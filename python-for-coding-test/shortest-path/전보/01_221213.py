c549c5b4dd062b3fce56c30402508c8872ff8e95
DADA YO <dayeon.yoon@gmail.com>
2022-12-13 01:53:29 +0900

    전보

---
+++ python-for-coding-test/shortest-path/전보/01_221213.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m, c = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+INF = 1e9
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+  x, y, z = map(int, input().split())
+  graph[x].append((y, z))
+
+q = []
+heapq.heappush(q, (0, c))
+distance[c] = 0
+
+while q:
+  dist, now = heapq.heappop(q)
+  if distance[now] < dist:
+    continue
+  for i in graph[now]:
+    cost = dist + i[1]
+    if cost < distance[i[0]]:
+      distance[i[0]] = cost
+      heapq.heappush(q, (cost, i[0]))
+
+cnt = 0
+max_time = 0
+for i in distance:
+  if i != INF and i != 0:
+    cnt += 1
+    max_time = max(i, max_time)
+
+print(cnt, max_time)
