ad5ad52105439e0a2d29968a9a8aae83bb7ffc00
DADA YO <dayeon.yoon@gmail.com>
2023-01-05 01:15:55 +0900

    탑승구

---
+++ python-for-coding-test/graph/탑승구/01_230105.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+g = int(input())
+p = int(input())
+
+parent = [i for i in range(g + 1)]
+
+def find_parent(parent, x):
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+
+ans = 0
+for _ in range(1, p + 1):
+  data = find_parent(parent, int(input()))
+  if data == 0:
+    break
+  union_parent(parent, data, data - 1)
+  ans += 1
+
+print(ans)
