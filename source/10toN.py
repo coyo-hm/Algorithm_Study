fdf13cc236d13ca6895f6b4609db3f71ad74c57a
DADA YO <dayeon.yoon@gmail.com>
2023-03-06 00:22:32 +0900

    10 to N

---
+++ source/10toN.py
@@ -0,0 +1,9 @@
+import string
+
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0 :
+        return tmp[r] 
+    else :
+        return convert(q, base) + tmp[r]
