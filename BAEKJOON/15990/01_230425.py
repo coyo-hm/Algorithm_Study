4a41ef53ffef1262acd7a6ac3bd59268bd78cb4f
COYO <dayeon.yoon@gmail.com>
2023-04-25 22:14:46 +0900

    BAEKJOON: 15990

---
+++ BAEKJOON/15990/01_230425.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+m = 100001
+dp = [[0, 0, 0] for _ in range(m)]
+dp[1] = [1, 0, 0]
+dp[2] = [0, 1, 0]
+dp[3] = [1, 1, 1]
+
+for i in range(4, m):
+    dp[i][0] = (dp[i - 1][1] + dp[i - 1][2])% 1000000009
+    dp[i][1] =( dp[i - 2][0] + dp[i - 2][2])% 1000000009
+    dp[i][2] = (dp[i - 3][0] + dp[i - 3][1])% 1000000009
+
+for _ in range(t):
+    n = int(input())
+    # print(dp[n])
+    print(sum(dp[n]) % 1000000009)
+
