d908938072426408a8a503460f7401e095c0148a
COYO <dayeon.yoon@gmail.com>
2023-04-29 00:06:35 +0900

    BAEKJOON: 2785

--- BAEKJOON/2785/01_230428.py
+++ BAEKJOON/2785/01_230428.py
@@ -2,12 +2,27 @@ import sys
 import collections
 input = sys.stdin.readline
 n = int(input())
-ls = collections.deque(list(map(int, input().split())))
+ls = list(map(int, input().split()))
 ls.sort()
+ls = collections.deque(ls)
 cnt = 0
 
-
-
-
+while True:
+    g = ls.popleft()
+    l = len(ls)
+    if g == 1:
+        a = ls.pop()
+        b = ls.pop()
+        ls.append(a + b + g)
+        cnt += 1
+    elif l > g:
+        cnt += g
+        a = g
+        for i in range(g + 1):
+            a += ls.pop()
+        ls.append(a)
+    else:
+        cnt += l
+        break
 
 print(cnt)
