f523504ecdf52d99b292f953adec234048193952
COYO <dayeon.yoon@gmail.com>
2023-05-07 22:56:37 +0900

    PROGRAMMERS:가장 큰 수

---
+++ PROGRAMMERES/정렬/가장 큰 수/01_230507.py
@@ -0,0 +1,19 @@
+from functools import cmp_to_key
+
+
+def compare(a, b):
+    sa = str(a)
+    sb = str(b)
+    return int(sb + sa) - int(sa + sb)
+
+
+def solution(numbers):
+    answer = ""
+    arr = sorted(numbers, key=cmp_to_key(compare))
+
+    if arr[0] == 0:
+        return "0"
+
+    for n in arr:
+        answer += str(n)
+    return answer
