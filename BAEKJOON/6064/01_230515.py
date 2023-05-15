4f2fca7be566de4069f7d8d1c07f9b1a022b2b23
COYO <dayeon.yoon@gmail.com>
2023-05-15 21:38:36 +0900

    BAEKJOON: 6064

---
+++ BAEKJOON/6064/01_230515.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+
+
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+for _ in range(t):
+    m, n, x, y = map(int, input().split())
+    g = lcm(max(m, n), min(m, n))
+    res = -1
+    for i in range(g // m + 1):
+        if (m * i + x - y) % n == 0:
+            res = m * i + x
+            break
+    print(res)
