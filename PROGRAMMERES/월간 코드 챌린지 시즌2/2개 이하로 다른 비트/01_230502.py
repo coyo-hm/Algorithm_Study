b18736110dcb2fa2b6f84d18d75b73c6b39bc7af
COYO <dayeon.yoon@gmail.com>
2023-06-04 00:15:24 +0900

    PROGRAMMERS: 2개 이하로 다른 비트

--- PROGRAMMERES/월간 코드 챌린지 시즌2/2개 이하로 다른 비트/01_230502.py
+++ PROGRAMMERES/월간 코드 챌린지 시즌2/2개 이하로 다른 비트/01_230502.py
@@ -1,1 +1,16 @@
-temp
+# https://school.programmers.co.kr/learn/courses/30/lessons/77885#
+def func(number):
+    b = "00" + bin(number)[2:]
+    l = len(b)
+    for i in range(l - 1, -1, -1):
+        if b[i] == "0":
+            c = 2 ** (l - 1 - i)
+            return number + c - c // 2
+
+
+def solution(numbers):
+    answer = []
+    for n in numbers:
+        ans = n + 1 if n % 2 == 0 else func(n)
+        answer.append(ans)
+    return answer
