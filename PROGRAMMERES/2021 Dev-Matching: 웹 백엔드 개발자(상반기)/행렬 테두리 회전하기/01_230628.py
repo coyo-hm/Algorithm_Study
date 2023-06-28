62a43849b46c4afc8fdaa37f50c8840e1c67c89c
COYO <dayeon.yoon@gmail.com>
2023-06-28 23:56:22 +0900

    PROGRAMMERS: 행렬 테두리 회전하기

--- PROGRAMMERES/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/행렬 테두리 회전하기/01_230628.py
+++ PROGRAMMERES/2021 Dev-Matching: 웹 백엔드 개발자(상반기)/행렬 테두리 회전하기/01_230628.py
@@ -1,15 +1,37 @@
 def rotate(sx, sy, ex, ey, graph):
-    m = int(INF)
-    t = graph[sx - 1][sy]
-    for y in range(sy - 1, ey - 1):
-        graph[]
-
-    for x in range(sx, ex - 1):
-        for y
-
+    a = graph[sx][sy - 1]
+    m = a
+    for y in range(sy - 1, ey):
+        b = graph[sx - 1][y]
+        graph[sx - 1][y] = a
+        a = b
+        m = min(m, b)
+    if len(graph) > 2:
+        for x in range(sx, ex - 1):
+            b = graph[x][ey - 1]
+            graph[x][ey - 1] = a
+            a = b
+            m = min(m, b)
+    for y in range(ey - 1, sy - 2, -1):
+        b = graph[ex - 1][y]
+        graph[ex - 1][y] = a
+        a = b
+        m = min(m, b)
+    if len(graph) > 2:
+        for x in range(ex - 2, sx - 2, -1):
+            b = graph[x][sy - 1]
+            graph[x][sy - 1] = a
+            a = b
+            m = min(m, b)
+    # print(m)
+    # for row in graph:
+        # print(row)
+    return [m, graph]
 
 def solution(rows, columns, queries):
     answer = []
-    graph = [[c + r * rows + 1 for c in range(columns)] for r in range(rows)]
-    print(graph)
+    graph = [[c + r * columns + 1 for c in range(columns)] for r in range(rows)]
+    for sx, sy, ex, ey in queries:
+        [m, graph] = rotate(sx, sy, ex, ey, graph)
+        answer.append(m)
     return answer
