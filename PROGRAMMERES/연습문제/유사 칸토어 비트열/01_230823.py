f6264a33501e5f0fd5621959e66c0259a0a023e1
COYO <dayeon.yoon@gmail.com>
2023-08-23 01:22:06 +0900

    PROGRAMMERS: 유사 칸토어 비트열(fail)

--- PROGRAMMERES/연습문제/유사 칸토어 비트열/01_230823.py
+++ PROGRAMMERES/연습문제/유사 칸토어 비트열/01_230823.py
@@ -1,7 +1,12 @@
 def solution(n, l, r):
     answer = 0
-    dp = ["1"] * (n + 1)
-    for i in range(1, n + 1):
-        dp[i] = dp[i - 1].replace("0", "00000").replace("1","11011")[:r]
-    print(dp[n])
-    return dp[n][l - 1:r].count("1")
+    # dp = ["1"] * 21
+    # n = 4
+    # for i in range(1, n + 1):
+    #     dp[i] = dp[i - 1].replace("0", "00000").replace("1","11011")
+    # print(dp[n])
+    for i in range(l - 1, r):
+        # print(i, i % 5, i // (5 ** (n - 1)), (i // 5) % 5)
+        if i % 5 != 2 and i // (5 ** (n - 1)) != 2 and (i // 5) % 5 != 2:
+            answer += 1
+    return answer
