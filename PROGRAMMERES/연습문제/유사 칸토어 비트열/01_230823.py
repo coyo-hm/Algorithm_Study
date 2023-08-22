553971e9d7596e729522a9e876abff873c99af11
COYO <dayeon.yoon@gmail.com>
2023-08-23 01:34:30 +0900

    PROGRAMMERS: 유사 칸토어 비트열

--- PROGRAMMERES/연습문제/유사 칸토어 비트열/01_230823.py
+++ PROGRAMMERES/연습문제/유사 칸토어 비트열/01_230823.py
@@ -1,12 +1,8 @@
 def solution(n, l, r):
     answer = 0
-    # dp = ["1"] * 21
-    # n = 4
-    # for i in range(1, n + 1):
-    #     dp[i] = dp[i - 1].replace("0", "00000").replace("1","11011")
-    # print(dp[n])
     for i in range(l - 1, r):
-        # print(i, i % 5, i // (5 ** (n - 1)), (i // 5) % 5)
-        if i % 5 != 2 and i // (5 ** (n - 1)) != 2 and (i // 5) % 5 != 2:
+        while i % 5 != 2 and i > 0:
+            i = i // 5
+        if i == 0:
             answer += 1
     return answer
