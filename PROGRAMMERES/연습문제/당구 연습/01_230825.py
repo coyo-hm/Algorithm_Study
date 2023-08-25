d1b5bc1d0183b5f6d9498db6e6c14bf9bfa03128
COYO <dayeon.yoon@gmail.com>
2023-08-26 00:22:45 +0900

    PROGRAMMERS: 당구 연습

--- PROGRAMMERES/연습문제/당구 연습/01_230825.py
+++ PROGRAMMERES/연습문제/당구 연습/01_230825.py
@@ -1,11 +1,19 @@
 def calcDis(x1, y1, x2, y2):
-    return (x1 - x2) ** 2 + (y1 - y2) ** 2
-
+    return int((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 def minDis(m, n, startX, startY, endX, endY):
-    if
-
+    INF = float('inf')
+    u, d, l, r = INF, INF, INF, INF
+    if not(startX == endX and startY < endY):
+        u = calcDis(startX, startY, endX, 2 * n - endY)
+    if not(startX == endX and startY > endY):
+        d = calcDis(startX, startY, endX, -endY)
+    if not(startX > endX and startY == endY):
+        l = calcDis(startX, startY, -endX, endY)
+    if not(startX < endX and startY == endY):
+        r = calcDis(startX, startY, 2 * m - endX, endY)
+    return min(u, d, l, r)
 
 def solution(m, n, startX, startY, balls):
-    answer = []
+    answer = [minDis(m, n, startX, startY, endX, endY) for [endX, endY] in balls]
     return answer
