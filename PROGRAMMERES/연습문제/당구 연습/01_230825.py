7659209156a2eeeead5bf553bf272043e05a45c6
COYO <dayeon.yoon@gmail.com>
2023-08-25 23:55:04 +0900

    PROGRAMMERS: 당구 연습

---
+++ PROGRAMMERES/연습문제/당구 연습/01_230825.py
@@ -0,0 +1,11 @@
+def calcDis(x1, y1, x2, y2):
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+
+def minDis(m, n, startX, startY, endX, endY):
+    if
+
+
+def solution(m, n, startX, startY, balls):
+    answer = []
+    return answer
