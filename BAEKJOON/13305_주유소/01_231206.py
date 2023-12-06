316c6f0ccf5d860a4ae6a0bb776fe5362054ecbb
COYO <dayeon.yoon@gmail.com>
2023-12-06 19:45:17 +0900

    BOJ: 13305_주유소

---
+++ BAEKJOON/13305_주유소/01_231206.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+road = list(map(int, input().split()))
+gas = list(map(int, input().split()))
+
+min_gas = gas[0]
+answer = 0
+
+for i, r in enumerate(road):
+    answer += r * min_gas
+    if gas[i + 1] < min_gas:
+        min_gas = gas[i + 1]
+
+print(answer)
