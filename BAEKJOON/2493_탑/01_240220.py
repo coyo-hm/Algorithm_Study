812fd69dd602d0e4653dd6a7fd716e8cc2266d56
COYO <dayeon.yoon@gmail.com>
2024-02-21 00:09:23 +0900

    boj: 2493_탑

---
+++ BAEKJOON/2493_탑/01_240220.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/2493
+# 00:33:28
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+tops = list(map(int, input().split()))
+receiver = [0] * n
+stack = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > tops[i]:
+            receiver[i] = stack[-1][0] + 1
+            break
+        else:
+            stack.pop()
+    stack.append([i, tops[i]])
+
+print(*receiver)
