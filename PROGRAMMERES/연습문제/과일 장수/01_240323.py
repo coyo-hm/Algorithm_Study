4f1e31bb30a68b7bce344998dd12ebf87642c12e
COYO <dayeon.yoon@gmail.com>
2024-03-23 00:33:16 +0900

    programmers: 과일 장수

---
+++ PROGRAMMERES/연습문제/과일 장수/01_240323.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/135808
+# 00:07:58
+
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse = True)
+    for i in range(m - 1, len(score), m):
+        answer += score[i] * m
+    return answer
