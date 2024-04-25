79d74f60b3f0a0ead8928888a5e55bbc7818effb
COYO <dayeon.yoon@gmail.com>
2024-04-25 19:58:24 +0900

    boj: 14719_빗물

--- BAEKJOON/14719_빗물/02_240425.py
+++ BAEKJOON/14719_빗물/02_240425.py
@@ -1,22 +1,23 @@
 # https://www.acmicpc.net/problem/14719
-# 00:25:29
+# 00:13:32
 
 import sys
 
 input = sys.stdin.readline
-full_h, full_w = map(int, input().split())
+total_h, total_w = map(int, input().split())
 blocks = list(map(int, input().split()))
 sorted_blocks = sorted([(h, idx) for idx, h in enumerate(blocks)], reverse=True)
-answer = [0] * full_w
-prev = 0
+answer = [0] * total_w
 
-for idx in range(1, full_w):
-    h = min(sorted_blocks[prev][0], sorted_blocks[idx][0])
-    left_x = min(sorted_blocks[prev][1], sorted_blocks[idx][1])
-    right_x = max(sorted_blocks[prev][1], sorted_blocks[idx][1])
+prev_idx = 0
+for idx in range(1, total_w):
+    h = min(sorted_blocks[prev_idx][0], sorted_blocks[idx][0])
+    left_x = min(sorted_blocks[prev_idx][1], sorted_blocks[idx][1])
+    right_x = max(sorted_blocks[prev_idx][1], sorted_blocks[idx][1])
     for x in range(left_x, right_x + 1):
         answer[x] = max(h - blocks[x], answer[x])
     prev = idx
+
 
 # print(answer)
 print(sum(answer))
