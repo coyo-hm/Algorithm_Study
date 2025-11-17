f2c545aa4a83bc852139f394ec3c7e8539f568c0
COYO <dayeon.yoon@gmail.com>
2025-11-17 18:36:51 +0900

    solve: [swexpert] 1933

---
+++ swexpert/1933/01_251117.py
@@ -0,0 +1,8 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5PhcWaAKIDFAUq&probBoxId=AV9gdM_anw0DFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part1&problemBoxCnt=20
+# 00:02:14
+
+n = int(input())
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i, end=" ")
