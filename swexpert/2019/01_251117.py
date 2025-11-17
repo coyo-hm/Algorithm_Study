03033b01c3ca33d4e18be99492f305185f8414b2
COYO <dayeon.yoon@gmail.com>
2025-11-17 18:27:24 +0900

    solve: [swexpert] 2019

---
+++ swexpert/2019/01_251117.py
@@ -0,0 +1,5 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5QDEX6AqwDFAUq&probBoxId=AV9gdM_anw0DFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part1&problemBoxCnt=20
+
+n = int(input())
+for i in range(n + 1):
+    print(2 ** i, end=" ")
