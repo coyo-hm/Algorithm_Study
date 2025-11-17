c3a36d567a4a2ae969ae820910fc01769737d898
COYO <dayeon.yoon@gmail.com>
2025-11-17 17:44:01 +0900

    solve: [swexpert] 2063

---
+++ swexpert/2063/01_251117.py
@@ -0,0 +1,9 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5QPsXKA2UDFAUq&probBoxId=AV9gdM_anw0DFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part1&problemBoxCnt=20
+
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5QQhbqA4QDFAUq&probBoxId=AV9gdM_anw0DFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part1&problemBoxCnt=20
+# 00:05:40
+
+n = int(input())
+nums = list(map(int, input().split(" ")))
+nums.sort()
+print(nums[n // 2])
