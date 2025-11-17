3076ce94936934f0e94b832d059886341bb0ea26
COYO <dayeon.yoon@gmail.com>
2025-11-17 17:37:22 +0900

    solve: [swexpert] 2068

---
+++ swexpert/2068/01_251117.py
@@ -0,0 +1,8 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5QQhbqA4QDFAUq&probBoxId=AV9gdM_anw0DFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part1&problemBoxCnt=20
+# 00:01:17
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    ans = max(list(map(int, input().split(" "))))
+    print("#{0}".format(test_case), ans)
