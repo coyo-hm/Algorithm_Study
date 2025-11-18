670102bbc8f7d54c5650eaa7b2ce365b7d034150
COYO <dayeon.yoon@gmail.com>
2025-11-19 01:13:27 +0900

    solve: [swexpert] 1966

---
+++ swexpert/1966/01_251118.py
@@ -0,0 +1,9 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5PrmyKAWEDFAUq&probBoxId=AV9oaSMa3DEDFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part2&problemBoxCnt=14
+# 00:01:50
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    nums.sort()
+    print("#{0}".format(test_case), " ".join(map(str, nums)))
