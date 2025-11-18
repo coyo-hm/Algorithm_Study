693dc554e8a8aeb1c39442365577845889ff2fce
COYO <dayeon.yoon@gmail.com>
2025-11-18 15:25:22 +0900

    solve: [swexpert] 1986

---
+++ swexpert/1986/01_251118.py
@@ -0,0 +1,14 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV5PxmBqAe8DFAUq&probBoxId=AV9oaSMa3DEDFAQc&type=PROBLEM&problemBoxTitle=%5BD1%7ED2+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part2&problemBoxCnt=14
+# 00:01:50
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    ans = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            ans -= i
+        else:
+            ans += i
+
+    print("#{0}".format(test_case), ans)
