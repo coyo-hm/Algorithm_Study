9d7fd067194e6907ef72d403b134b1c98e114665
COYO <dayeon.yoon@gmail.com>
2025-11-20 04:03:30 +0900

    solve: [swexpert] 1285

--- swexpert/1285/01_251120.py
+++ swexpert/1285/01_251120.py
@@ -1,4 +1,6 @@
 # https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV18-stqI8oCFAZN&probBoxId=AV-HZfeqN3ADFASP&type=PROBLEM&problemBoxTitle=%5BD2%7ED3+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part3&problemBoxCnt=14
+# 00:06:33
+
 
 T = int(input())
 for test_case in range(1, T + 1):
