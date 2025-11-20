9d4a8ca9e62141b02538e972b6529bb005b15463
COYO <dayeon.yoon@gmail.com>
2025-11-20 23:10:30 +0900

    solve: [swexpert] 1217

---
+++ swexpert/1217/01_251120.py
@@ -0,0 +1,8 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AV6kld8aisgDFASb&contestProbId=AV14dUIaAAUCFAYD&probBoxId=AV-4MojKLNADFATz&type=PROBLEM&problemBoxTitle=%5BD2%7ED3+%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4%5D+%EA%B8%B0%EC%B4%88+%EB%8B%A4%EC%A7%80%EA%B8%B0+Part4&problemBoxCnt=14
+# 00:02:25
+
+T = 10
+for _ in range(1, T + 1):
+    test_case = int(input())
+    n, m = map(int, input().split())
+    print("#" + str(test_case), n ** m)
